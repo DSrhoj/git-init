f1245e884ac16d0b16c72be5092f5aedbd46bf19
DSrhoj <33469261+DSrhoj@users.noreply.github.com>
2020-10-20 15:49:22 +0200

    shit_login

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -11,29 +11,58 @@ namespace App1
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TextView smrad;
+            TextView smrad, smrad2;
             RelativeLayout RL2, RL3;
+            EditText ET1, ET2, ET3;
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            
-            smrad = FindViewById<TextView>(Resource.Id.tv) ;
+
+            ET1 = FindViewById<EditText>(Resource.Id.editText1);
+            ET2 = FindViewById<EditText>(Resource.Id.editText2);
+            ET3 = FindViewById<EditText>(Resource.Id.editText3);
+            smrad = FindViewById<TextView>(Resource.Id.tv);
+            smrad2 = FindViewById<TextView>(Resource.Id.tv2);
+            smrad2.Visibility = Android.Views.ViewStates.Invisible;
             RL3 = FindViewById<RelativeLayout>(Resource.Id.relativeLayout3);
+            RL2 = FindViewById<RelativeLayout>(Resource.Id.relativeLayout2);
             RL3.Visibility = Android.Views.ViewStates.Invisible;
             FindViewById<Button>(Resource.Id.toni).Click += (o, e) =>
             {
-                if (smrad.Visibility == Android.Views.ViewStates.Invisible)
+                if (smrad.Visibility == Android.Views.ViewStates.Visible)//smrad.Visibility == Android.Views.ViewStates.Invisible
                 {
                     //smrad.Visibility = Android.Views.ViewStates.Visible;
-                    RL2 = FindViewById<RelativeLayout>(Resource.Id.relativeLayout2);
+                    
                     RL2.Visibility = Android.Views.ViewStates.Invisible;
                     RL3.Visibility = Android.Views.ViewStates.Visible;
                 }
 
+                //else
+                //{
+                //    smrad.Visibility = Android.Views.ViewStates.Invisible;
+                //}
+
+            };
+
+            FindViewById<Button>(Resource.Id.toni2).Click += (o, e) =>
+            {
+                if (string.IsNullOrEmpty(ET1.Text.ToString()) || string.IsNullOrEmpty(ET2.Text.ToString()) || string.IsNullOrEmpty(ET3.Text.ToString()))
+                {
+                    smrad2.Visibility = Android.Views.ViewStates.Visible;
+                }
                 else
                 {
-                    smrad.Visibility = Android.Views.ViewStates.Invisible;
+                    smrad2.Visibility = Android.Views.ViewStates.Invisible;
+                    RL2.Visibility = Android.Views.ViewStates.Visible;
+                    RL3.Visibility = Android.Views.ViewStates.Invisible;
+                    smrad.Visibility = Android.Views.ViewStates.Visible;
+                    smrad.TextSize = 50; 
+                    smrad.Text = "Svejedno smrdi";
+                    ET1.Text = "";
+                    ET2.Text = "";
+                    ET3.Text = "";
+
                 }
 
             };
--- App1/obj/Debug/90/android/environment.x86_64.s
+++ App1/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"f4ff1cf9-bf8d-4d07-8caa-f5d7e1d20ba1"
+	.asciz	"92553743-6cfa-4604-aa54-d7f82d3357fc"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
